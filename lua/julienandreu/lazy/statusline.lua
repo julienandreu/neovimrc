c2d9a3fa1c8dc9eae11df3b4ae3cef163691b25d
Julien Andreu <julienandreu@me.com>
2024-07-02 23:01:52 -0400

    feat: Simplify plugins usage

--- lua/julienandreu/lazy/statusline.lua
+++ lua/julienandreu/lazy/statusline.lua
@@ -1,7 +1,17 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = true,
+        setup = function()
+            options = {
+                theme = "horizon"
+            }
+        end
+    },
 }
