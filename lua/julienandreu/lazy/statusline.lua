fb179e920310fb4785485475838da83d3e654eea
Julien Andreu <julienandreu@me.com>
2024-07-03 05:52:42 -0400

    feat: Add LSP and formatter

--- lua/julienandreu/lazy/statusline.lua
+++ lua/julienandreu/lazy/statusline.lua
@@ -1,33 +1,24 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = true,
-        setup = function()
-            options = {
-                theme = "horizon"
-            }
-        end
-    },
-    {
-        'mawkler/modicator.nvim',
-        init = function()
-            -- These are required for Modicator to work
-            vim.o.cursorline = true
-            vim.o.number = true
-            vim.o.termguicolors = true
-        end,
-        opts = {
-            -- Warn if any required option above is missing. May emit false positives
-            -- if some other plugin modifies them, which in that case you can just
-            -- ignore. Feel free to remove this line after you've gotten Modicator to
-            -- work properly.
-            show_warnings = true,
-        }
-    }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = true,
+	},
+	{
+		"mawkler/modicator.nvim",
+		init = function()
+			-- These are required for Modicator to work
+			vim.o.cursorline = true
+			vim.o.number = true
+			vim.o.termguicolors = true
+		end,
+		opts = {
+			show_warnings = true,
+		},
+	},
 }
