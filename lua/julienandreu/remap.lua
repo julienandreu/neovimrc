c826abf1f30ddc3d73bec91e24cf10e5edf698ce
Julien Andreu <julienandreu@me.com>
2024-07-03 06:00:47 -0400

    feat: Add git blame

--- lua/julienandreu/remap.lua
+++ lua/julienandreu/remap.lua
@@ -1,9 +1,9 @@
-
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+vim.keymap.set("n", "<leader>gb", "<CMD>GitBlameToggle<CR>", { desc = "Git blame toggled" })
