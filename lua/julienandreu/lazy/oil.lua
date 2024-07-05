6914d1d9ba7b86d2c951fc3eb91395d0923f4cbb
Julien Andreu <julienandreu@me.com>
2024-07-04 21:51:51 -0400

    feat: Update dependencies for Lazy plugins; add Telescope plugin and key mappings for improved navigation

--- lua/julienandreu/lazy/oil.lua
+++ lua/julienandreu/lazy/oil.lua
@@ -13,7 +13,7 @@ return {
 					show_hidden = true,
 					natural_order = true,
 					is_always_hidden = function(name, _)
-						return name == ".." or name == ".git"
+						return name == ".." or name == ".git" or name == "__pycache__" or name == ".venv"
 					end,
 				},
 				win_options = {
--- lua/julienandreu/remap.lua
+++ lua/julienandreu/remap.lua
@@ -1,9 +1,20 @@
 vim.g.mapleader = " "
-
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
 
+-- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+-- Git
 vim.keymap.set("n", "<leader>gb", "<CMD>GitBlameToggle<CR>", { desc = "Git blame toggled" })
+
+-- Telescope
+vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", {})
+vim.keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", {})
+vim.keymap.set("n", "<leader>fb", "<CMD>Telescope buffers<CR>", {})
+vim.keymap.set("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", {})
+vim.keymap.set("n", "<leader>fu", "<CMD>Telescope lsp_references<CR>", {})
+vim.keymap.set("n", "<leader>fd", "<CMD>Telescope lsp_definitions<CR>", {})
+vim.keymap.set("n", "<leader>fds", "<CMD>Telescope lsp_document_symbols<CR>", {})
+vim.keymap.set("n", "<leader>fws", "<CMD>Telescope lsp_workspace_symbols<CR>", {})
